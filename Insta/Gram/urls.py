7c8c198f0c6afd0b90fde8a37faf2784e46f110c
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-13 20:59:59 +0300

    Add search bar

--- Insta/Gram/urls.py
+++ Insta/Gram/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns=[
     url(r'^$',views.latest_images,name='latest'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
--- Insta/Gram/views.py
+++ Insta/Gram/views.py
@@ -13,3 +13,16 @@ def latest_images(request):
     news = Image.todays_news()
 
     return render(request, 'allofinsta/base.html', {"date": date,})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'allofinsta/search.html',{"message":message,"articles": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'allofinsta/search.html',{"message":message})
